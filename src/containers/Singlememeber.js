a8cc403e1389f5b65f82a858355ff42aae63867a
najiba chaoua <61496396+najibachaawa@users.noreply.github.com>
2020-03-08 18:34:28 +0100

    Add files via upload

--- src/containers/Singlememeber.js
+++ src/containers/Singlememeber.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-
+import {Button,Card,CardBody} from 'reactstrap'
   export default class SingleMember extends React.Component{
-      constructor(props){
-          super(props)
-          
+    state= {
+         
+    member: {
+        id: 1,
+        name: "Natarajah",
+        phoneNumber: "0938073320",
+        status:"actif",
+        topic:"topic1",
+        image: "https://s3.amazonaws.com/uifaces/faces/twitter/dvdwinden/128.jpg",
       }
-render(){
-
-    return(
-        <div>
-              {this.props.name}
-        </div>
+    }
       
-    )
+    
+render(){
+   let member=this.state.member
+    return (
+         <Card
+        hoverable
+        style={{ width: 240 }}>
+       
+      
+           {<img  src={member.image} />}
+
+           <h1>{member.name}</h1> 
+            <br></br>
+             <h2>Spécialité : {member.topic}</h2>
+             <Button type="primary"> envoyer un message</Button>
+      </Card>
+      );
+        
+             
+
+
+
+
 }
-
-
-
-
 }
